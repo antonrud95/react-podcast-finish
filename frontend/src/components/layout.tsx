c423ff646aac4244aed8dada3a85693f2ac70ae7
arud95 <antonrud95@yandex.ru>
2020-10-17 01:51:06 +0300

    feature: add top and bottom stars

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -2,11 +2,40 @@ import React from 'react'
 import Navbar from '~/components/ui/general/navbar/navbar.component'
 import '~/styles/index.scss'
 
+import Img from 'gatsby-image'
+import { graphql, useStaticQuery } from 'gatsby'
+
 const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+  query {
+    topStars: file(relativePath: { eq: "images/hero/top-image.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1440, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    bottomStars: file(relativePath: { eq: "images/hero/bottom-image.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1440, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`)
   return (
     <>
       <Navbar />
       <main>{children}</main>
+      <Img
+        fluid={data.topStars.childImageSharp.fluid}
+        className="top"
+      />
+      <Img
+        fluid={data.bottomStars.childImageSharp.fluid}
+        className="bot"
+      />
     </>
   )
 }
