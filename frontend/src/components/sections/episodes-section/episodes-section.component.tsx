09a49684c02e76cd6df3d96fc00cc388aebcb829
arud95 <antonrud95@yandex.ru>
2020-10-17 13:13:48 +0300

    feature: add mobile version episodes cards

--- frontend/src/components/sections/episodes-section/episodes-section.component.tsx
+++ frontend/src/components/sections/episodes-section/episodes-section.component.tsx
@@ -6,28 +6,46 @@ import EpisodesItem from '../../ui/general/episodes-item/episodes-item.component
 import FC from '~/types/fc'
 import SButton from '../../ui/general/button/button.component'
 
+import { useWindowDimensions } from '~/hooks/useWindowDimensions';
+
 interface Props {
     episodes: EpisodesType[]
 }
 
 const EpisodesSection: FC<Props> = ({episodes}) => {
+    const { width } = useWindowDimensions();
     return (
         <React.Fragment>
             <h2 className={styles.episodesMainTitle}>All episodes</h2>
             <div className={styles.episodesContainer}>
-                {episodes.map((episode: EpisodesType, index) => {
-                    return (
-                        <EpisodesItem 
-                            key={episode.id}
-                            title={episode.title}
-                            text={episode.text}
-                        />
-                    )
-                })}
+                { width <= 767 ? episodes.map((episode: EpisodesType, index) => {
+                    if (index > 2) {
+                        return
+                    } else {
+                        return (
+                            <EpisodesItem 
+                                key={episode.id}
+                                title={episode.title}
+                                text={episode.text}
+                            />
+                            )
+                        }
+                    }) :
+                    episodes.map((episode: EpisodesType, index) => {
+                        return (
+                            <EpisodesItem 
+                                key={episode.id}
+                                title={episode.title}
+                                text={episode.text}
+                            />
+                        )
+                    })
+                }
+                
             </div>
-            <div className={styles.episodesBtn}>
-                <SButton variant="section">View all episodes</SButton>
-            </div>
+            {width <= 767 && 
+                <SButton variant="section" className={styles.episodesBtn}>View all episodes</SButton>
+            }
         </React.Fragment>
     )
 }
